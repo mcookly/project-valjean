172e57abf4e15388aa3fad49398d60bcfb4bfce8
Max Cook <maxtcook@gmail.com>
2021-07-27 10:46:29 -0700

    Finished NDH Spider! Yes!

--- app/food_crawler/spiders/ndh_spider.py
+++ app/food_crawler/spiders/ndh_spider.py
@@ -1,13 +1,42 @@
 import scrapy
-from base64 import b64decode
+import logging
 from datetime import date
 from scrapy_splash.request import SplashRequest
+from scrapy.selector import Selector
+
+def extract_food_cat(html_text):
+    """
+    Input: html text
+    Output: dict with keys as categories and values as lists of foods
+    """
+    html = Selector(text=html_text) # Creates a selector object for Xpath parsing.
+    # NOTE @Nick: feel free to change this data format to whatever works best.
+    foods_per_category = dict()
+    # Nodes of interest are all td
+    td_nodes = html.xpath('//table[@class="cbo_nn_itemGridTable"]/tbody/tr//td')
+
+    for node in td_nodes:
+        # Using extract_first() to avoid a list return
+        node_class = node.xpath('./@class').extract_first()
+        if node_class == 'cbo_nn_itemGroupRow':
+            # Found a category
+            category = node.xpath('./text()').extract_first()
+            foods_per_category[category] = list()
+            logging.debug(f'Found category: {category}')
+        elif node_class == 'cbo_nn_itemHover':
+            # Found a food item under a category
+            food_item = node.xpath('./text()').extract_first()
+            foods_per_category[category].append(food_item)
+            logging.debug(f'    Found food item: {food_item}')
+    
+    return foods_per_category
+
 
 # NOTE: Uses a flimsy method for relative paths and may break when using docker or
 # Google Cloud.
-with open('./food_crawler/lua_scripts/nav_to_dh_menu.lua', 'r') as f:
+with open('./food_crawler/scripts/nav_to_dh_menu.lua', 'r') as f:
     script_nav_to_dh_menu = f.read()
-with open('./food_crawler/lua_scripts/record_meal_elements.lua', 'r') as f:
+with open('./food_crawler/scripts/record_meal_elements.lua', 'r') as f:
     script_record_meal_elements = f.read()
 
 class NDHSPIDER(scrapy.Spider):
@@ -15,6 +44,9 @@ class NDHSPIDER(scrapy.Spider):
     allowed_domains = ['nutrition.nd.edu']
     # Both NDH and SDH spiders will start from this URL.
     url = 'http://nutrition.nd.edu/NetNutrition/1'
+    current_day = date.today().strftime('%A, %B %-d, %Y')
+
+    is_weekend = False if date.today().weekday() < 5 else True
 
     def start_requests(self):
         yield SplashRequest(
@@ -33,21 +65,13 @@ class NDHSPIDER(scrapy.Spider):
         # This extracts the current day's meals' CSS selectors for
         # the next extraction.
 
-        # filename = 'result_meal.html'
-        # visdebug_filename = 'log.png'
-        # debug_img = b64decode(response.data['png'])
-        CURRENT_DATE = date.today().strftime('%A, %B %-d, %Y')
-
-        # Find index for the current day
+        # Find node index for the current day
         for i in range(1, 8):
             day = response.css(f'div.cbo_nn_menuTableDiv tr:nth-child({i}) td.cbo_nn_menuCell td::text').get()
-            if day == CURRENT_DATE:
-                self.log(f'Found the current day ({day}) at index {i}.')
+            if day == self.current_day:
+                self.log(f'Found the current day ({day}) at node index {i}.')
                 break
-        self.log('Completed first parse.')
-        # with open(visdebug_filename, 'wb') as f:
-        #     f.write(debug_img)
-        # self.log(f'Saved file {filename}')
+        self.log(' --- Completed first parse. --- ')
         
         yield SplashRequest(
             url=self.url,
@@ -59,20 +83,25 @@ class NDHSPIDER(scrapy.Spider):
                 'dh': 'tr.cbo_nn_unitsAlternateRow:nth-child(2) > td:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > a:nth-child(1)',
                 'fwd_btn': '.cbo_nn_childUnitsCell > a:nth-child(1)',
                 'index': i,
-                'weekend': False
+                'weekend': self.is_weekend
             })
     
     def parse_meals(self, response):
-        # Parses the food items from each meal at NDH.
-        visdebug_filename = 'log_2.png'
-        rb = response.data
-        self.log(rb.keys())
-        with open('results.html', 'w') as f:
-            f.write(str(rb))
-        # debug_img = b64decode(response.data['png'])
-        # with open(visdebug_filename, 'wb') as f:
-        #     f.write(debug_img)
-        self.log('Completed second parse.')
+        # Parses the food items from each meal at NDH and organize
+        # into dicts for each meal.
 
-        categories = response.xpath('//td[@class="cbo_nn_itemGroupRow"]/text()').extract()
-        self.log(f'Categories: {categories}')
+        # Creates a dict: {key: meal, value: html from meal's webpage}
+        response_per_meal = response.data
+
+        # NOTE: Perhaps there is a more efficient way of categorizing
+        # the parses. Would a numerical system work more smoothly?
+        if self.is_weekend:
+            meal_brunch = extract_food_cat(response_per_meal['brunch'])
+        else:
+            meal_breakfast = extract_food_cat(response_per_meal['breakfast'])
+            meal_lunch = extract_food_cat(response_per_meal['lunch'])
+        meal_dinner = extract_food_cat(response_per_meal['dinner'])
+
+        # -------- @ Nick take over from here. ---------
+
+        self.log(' --- Completed second parse. --- ')
