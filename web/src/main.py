f6779451f2b87a41246bc404c46e4105f1fefbc7
Max Cook <maxtcook@gmail.com>
2021-08-02 22:14:57 -0700

    Added bootstrap

--- web/src/main.py
+++ web/src/main.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 # This file initiates the Flask app
 
-main = Flask(__name__)
-@main.route('/')
+app = Flask(__name__)
+@app.route('/')
 def index():
     return "Hello World!"
 
 if __name__ == "__main__":
-    main.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
