acdf757c79afeedd151eb1657c16321e6f3c46bf
Max Cook <maxtcook@gmail.com>
2021-08-03 14:35:56 -0700

    [add] error handling for 404 errors.
    404 redirects (303) to Home

--- web/src/main.py
+++ web/src/main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, redirect
+from flask.helpers import url_for
 
 # This line initiates the Flask app
 app = Flask(__name__)
@@ -7,22 +8,22 @@ app = Flask(__name__)
 ########### Index
 @app.route('/')
 def index():
-    return render_template('index.html', cuurent_page='Home')
+    return render_template('index.html')
 
 ########### Rate
 @app.route('/rate/')
 def rate():
-    return render_template('rate.html', cuurent_page='Rate')
+    return render_template('rate.html')
 
 ########### Stats
 @app.route('/stats/')
 def stats():
-    return render_template('stats.html', cuurent_page='Stats')
+    return render_template('stats.html')
 
 ########### About
 @app.route('/about/')
 def about():
-    return render_template('about.html', cuurent_page='About')
+    return render_template('about.html')
 
 ########### Load favicon
 @app.route('/favicon.ico')
@@ -30,6 +31,11 @@ def favicon():
     return send_from_directory(
         os.path.join(app.root_path, 'static/img/favicon'),
         'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+########### Error handling for missing pages
+@app.errorhandler(404)
+def handle_404(error):
+    return redirect(url_for('index'), 303)
 
 # Since this is the main script of the app, have it run itself.
 if __name__ == "__main__":
