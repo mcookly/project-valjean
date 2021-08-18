25f2d8af4c8975a1fe04b88be84b1f41e6b4e88b
Max Cook <maxtcook@gmail.com>
2021-08-18 16:40:45 -0700

    Move old scripts

--- scraper/food_crawler/foodhandler/foodhandler/communicator.py
+++
@@ -1,6 +0,0 @@
-### Send food data to SQL db
-def write_to_db():
-    """Write food data to db.
-    Takes: dict {category: [foods]}, returns: None
-    """
-    return None
--- scraper/food_crawler/foodhandler/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(name = 'Food Handler',
-    version = '0.1',
-    description = 'Additional tools to create a scalable food processor.',
-    author = 'Nicholas Clark (), Max Cook (mcookly)',
-    packages = ['foodhandler'],
-    zip_safe = False)
