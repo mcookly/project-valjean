443eec8ddc727fa9d9bbf0ffe07501d1b51b7634
Max Cook <maxtcook@gmail.com>
2021-08-28 19:05:52 -0400

    Fixed South throwing Lua error
    Changed concurrent requests in Scrapy settings to '1', which allows Splash to work properly.

--- scraper/food_crawler/settings.py
+++ scraper/food_crawler/settings.py
@@ -28,7 +28,8 @@ ROBOTSTXT_OBEY = True
 LOG_LEVEL = 'WARNING'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# NOTE: Must be kept at one for Scrapy-Splash to function properly.
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
--- scraper/food_crawler/spiders/dh_spider.py
+++ scraper/food_crawler/spiders/dh_spider.py
@@ -32,7 +32,7 @@ class DHSPIDER(scrapy.Spider):
     current_day = date.today().strftime('%A, %B %-d, %Y')
     meals_list = tuple()
     # Page selectors
-    wait_time = 3
+    wait_time = 1
     dining_hall_sel = {
         'South': 'tr.cbo_nn_unitsPrimaryRow:nth-child(5) > td:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > a:nth-child(1)',
         'North': 'tr.cbo_nn_unitsAlternateRow:nth-child(2) > td:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > a:nth-child(1)'
@@ -111,7 +111,7 @@ class DHSPIDER(scrapy.Spider):
                     cat_item['name'] = cat_name
                     cat_item['meal'] = meal
                     cat_item["dining_hall"] = response.meta.get('dh')
-                    
+
                     foods = set()
                     for j in range(i+1, len(td_nodes)):
                         # Cycle through future td's to find meals
