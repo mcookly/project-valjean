34e71c43069a08be9989c7b3bb7288ec361b78d3
Max Cook <maxtcook@gmail.com>
2021-10-01 00:57:53 -0400

    Fixed scraper processing
    - [fix] duplicate logging
    - [fix] amount of items being uploaded to DB. Last item was not being sent.
    - [change] algorithm for identifying categories and foods.

--- scraper/food_crawler/spiders/dh_spider.py
+++ scraper/food_crawler/spiders/dh_spider.py
@@ -1,36 +1,36 @@
 import os, logging, scrapy
-import google.cloud.logging
 from datetime import date
 from food_crawler.items import FoodCategory
 from scrapy_splash.request import SplashRequest
 from scrapy.selector import Selector
 
-### Open GCP logging client ###
-gcp_log_client = google.cloud.logging.Client()
-gcp_log_client.get_default_handler()
-gcp_log_client.setup_logging()
-
-### Load Lua scripts ###
-CUR_DIR = os.getcwd()
-SCRIPTS_DIR = os.path.join(CUR_DIR, 'food_crawler/scripts')
-
-### Load 'nav_to_dh_menu.lua'
-try:
-    with open(os.path.join(SCRIPTS_DIR, 'nav_to_dh_menu.lua'), 'r') as f:
-        script_nav_to_dh_menu = f.read()
-    logging.info('Found Lua script "nav_to_dh_menu" in ' + SCRIPTS_DIR)
-except:
-    logging.critical('Could not find Lua script "nav_to_dh_menu" in ' + SCRIPTS_DIR)
-
-### Load 'record_meal_elements.lua'
-try:
-    with open(os.path.join(SCRIPTS_DIR, 'record_meal_elements.lua'), 'r') as f:
-        script_record_meal_elements = f.read()
-    logging.info('Found Lua script "record_meal_elements" in ' + SCRIPTS_DIR)
-except:
-    logging.critical('Could not find Lua script "record_meal_elements" in ' + SCRIPTS_DIR)
-
 class DHSPIDER(scrapy.Spider):
+    ### __init__
+    def __init__(self, *args, **kwargs):
+        # Display target DNS for Splash
+        self.logger.debug('Using "' + os.environ.get('SPLASH_IP') + '" for Splash IP.')
+        
+        ## Load Lua scripts
+        CUR_DIR = os.getcwd()
+        SCRIPTS_DIR = os.path.join(CUR_DIR, 'food_crawler/scripts')
+
+        # Load 'nav_to_dh_menu.lua'
+        try:
+            with open(os.path.join(SCRIPTS_DIR, 'nav_to_dh_menu.lua'), 'r') as f:
+                self.script_nav_to_dh_menu = f.read()
+            self.logger.debug('Found Lua script "nav_to_dh_menu" in ' + SCRIPTS_DIR)
+        except:
+            self.logger.critical('Could not find Lua script "nav_to_dh_menu" in ' + SCRIPTS_DIR)
+
+        # Load 'record_meal_elements.lua'
+        try:
+            with open(os.path.join(SCRIPTS_DIR, 'record_meal_elements.lua'), 'r') as f:
+                self.script_record_meal_elements = f.read()
+            self.logger.debug('Found Lua script "record_meal_elements" in ' + SCRIPTS_DIR)
+        except:
+            self.logger.critical('Could not find Lua script "record_meal_elements" in ' + SCRIPTS_DIR)
+    
+    ## Init variables
     name = "dining_halls"
     allowed_domains = ['nutrition.nd.edu']
     # Both NDH and SDH spiders will start from this URL.
@@ -56,7 +56,7 @@ class DHSPIDER(scrapy.Spider):
                 dont_filter=True,
                 meta={'dh': dh},
                 args={
-                    'lua_source': script_nav_to_dh_menu,
+                    'lua_source': self.script_nav_to_dh_menu,
                     'wait': self.wait_time,
                     'timeout': self.splash_timeout,
                     'dh': self.dining_hall_sel[dh],
@@ -73,15 +73,14 @@ class DHSPIDER(scrapy.Spider):
         for i in range(1, 8):
             day = response.css(f'div.cbo_nn_menuTableDiv tr:nth-child({i}) td.cbo_nn_menuCell td::text').get()
             if day == self.current_day:
-                self.logger.info(f'[{dining_hall}]: Found the current day ({day}) at node index {i}.')
+                self.logger.debug(f'[{dining_hall}]: Found the current day ({day}) at node index {i}.')
                 break
         # Get list of meals for the day
         # '//a' is cycling through all child nodes under the current day node.
         self.meals_list = response.xpath(f'//*[@id="MenuList"]/div[2]/table/tbody/tr[{i}]/td/table/tbody/tr[2]/td/table/tbody/tr//a/text()').extract()
-        self.logger.info(f'[{dining_hall}]: Found {len(self.meals_list)} meal(s): {self.meals_list}')
+        self.logger.debug(f'[{dining_hall}]: Found {len(self.meals_list)} meal(s): {self.meals_list}')
         self.logger.info(f'[{dining_hall}]: Completed first parse')
 
-        self.logger.info(f'[{dining_hall}]: Beginning second parse...')
         yield SplashRequest(
             url=self.url,
             callback=self.parse_meals,
@@ -89,7 +88,7 @@ class DHSPIDER(scrapy.Spider):
             dont_filter=True,
             meta={'dh': dining_hall},
             args={
-                'lua_source': script_record_meal_elements,
+                'lua_source': self.script_record_meal_elements,
                 'wait': self.wait_time,
                 'timeout': self.splash_timeout,
                 'dh': self.dining_hall_sel[dining_hall],
@@ -100,40 +99,55 @@ class DHSPIDER(scrapy.Spider):
     
     def parse_meals(self, response):
         # Parses the food items from each meal and sends results through the
-        # item pipeline to be uploaded to Firebase.
+        # item pipeline to be uploaded to Firebase. (This triple for-loop
+        # is killing me, but is there any other way???)
 
         response_data = response.data
         xpath_tag = '//table[@class="cbo_nn_itemGridTable"]/tbody/tr//td'
         # Extract meal contents
         for meal in self.meals_list:
             meal_data = Selector(text=response_data[meal])
-            # For both DH, nodes of interest are all td
+            
+            cat_class = 'cbo_nn_itemGroupRow' # This designates a category
+            food_class = 'cbo_nn_itemHover'  # This designates a food
+
+            # For both DH, nodes of interest are all <td>
             td_nodes = meal_data.xpath(xpath_tag)
+            n_cats = td_nodes.xpath('./@class').extract().count(cat_class)
+            self.logger.info(f'[{response.meta.get("dh")}]: Found {n_cats} categories')
+            
 
-            for i in range(len(td_nodes)):
-                # Using extract_first() to avoid list return of a single item.
-                node = td_nodes[i]
-                if node.xpath('./@class').extract_first() == 'cbo_nn_itemGroupRow':
-                    # Found a category
-                    cat_item = FoodCategory() # Init category for item pipeline
-                    cat_name = node.xpath('./text()').extract_first()
-                    if cat_name[0] == '*':
-                        cat_name = cat_name[1:-1] # Trims '*' from category
-                    cat_item['name'] = cat_name
-                    cat_item['meal'] = meal
-                    cat_item["dining_hall"] = response.meta.get('dh')
+            for _ in range(n_cats):
+                # Loop only the number of categories
+                cat_node = td_nodes[0]
+                cat_name = cat_node.xpath('./text()').extract_first()
+                cat_item = FoodCategory() # Init category for item pipeline
 
-                    foods = set()
-                    for j in range(i+1, len(td_nodes)):
-                        # Cycle through future td's to find meals
-                        mini_node = td_nodes[j]
-                        mini_node_class = mini_node.xpath('./@class').extract_first()
-                        if mini_node_class == 'cbo_nn_itemHover':
-                            # Found a food item under a category
-                            foods.add(mini_node.xpath('./text()').extract_first())
-                        elif mini_node_class == 'cbo_nn_itemGroupRow':
-                            cat_item["foods"] = foods
-                            yield cat_item
-                            break # Stop at next category
+                self.logger.debug(f'Found category: {cat_name} in {meal}')
+
+                # Iterate through items
+                foods = set()
+                for i in range(1, len(td_nodes)):
+                    # Start one past the current category node
+                    node = td_nodes[i]
+                    node_class = node.xpath('./@class').extract_first()
+                    if node_class == food_class:
+                        # Found a food item under a category
+                        foods.add(node.xpath('./text()').extract_first())
+                    elif node_class == cat_class:
+                        # Found the next class
+                        break
+
+                # Prep and send item to pipeline  
+                if cat_name[0] == '*':
+                    cat_name = cat_name[1:-1] # Trims '*' from category
+                cat_item['name'] = cat_name
+                cat_item['meal'] = meal
+                cat_item["dining_hall"] = response.meta.get('dh')
+                cat_item["foods"] = foods
+                yield cat_item
+                  
+                # Prep for next loop
+                td_nodes = td_nodes[i:]
 
         self.logger.info(f'[{response.meta.get("dh")}]: Completed second parse')
